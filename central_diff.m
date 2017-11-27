46edc5d4a5225748acbc75419c3cb79f19b5f285
13carlosmata <cmata@kth.se>
2017-11-27 23:30:12 +0100

    Ltildes

---
+++ central_diff.m
@@ -0,0 +1,13 @@
+function [deltax,deltay] = central_diff(order)
+
+deltax = zeros(5,5);
+deltay = deltax;
+if order == 1
+    deltax = [-1/2,0,1/2];
+    deltay = [-1/2;0;1/2];
+
+elseif order == 2
+    deltax = [1,-2,1];
+    deltay = [1;-2;1];
+end
+end
