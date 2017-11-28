f940e8dc228c31f25210665859ba358b8f5c50c0
13carlosmata <cmata@kth.se>
2017-11-28 17:31:14 +0100

    central differences in matrices

--- central_diff.m
+++ central_diff.m
@@ -1,11 +1,13 @@
 function [deltax,deltay] = central_diff(order)
+deltax = zeros(5);
+deltay = zeros(5);
 
 if order == 1
-    deltax = [-1/2,0,1/2];
-    deltay = [-1/2;0;1/2];
+    deltax(3,2:4) = [-1/2,0,1/2];
+    deltay(2:4,3) = [-1/2;0;1/2];
 
 elseif order == 2
-    deltax = [1,-2,1];
-    deltay = [1;-2;1];
+    deltax(3,2:4) = [1,-2,1];
+    deltay(2:4,3) = [1;-2;1];
 end
 end
